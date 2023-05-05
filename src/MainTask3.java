920725ed47e542937470c777f43019257400d0cd
valensixx <valensixx@gmail.com>
2023-05-05 12:23:09 +0300

    task2 and task3 (GUI)

---
+++ src/MainTask3.java
@@ -0,0 +1,15 @@
+public class MainTask3 {
+    //Task 3
+    //expression => operands & operators
+    //operands => values, variables, numbers, quantity
+    //operators => + - * /
+    public static void main(String[] args){
+        int friends = 10;
+        friends = friends + 1; // it is the same as friends++;
+        System.out.println(friends);
+
+        double friendsGroupTwo = 10;   //here we convert int in to double so we can / by 3.
+        friendsGroupTwo = (double) friendsGroupTwo / 3;
+        System.out.println(friendsGroupTwo);
+    }
+}
