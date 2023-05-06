888bab7f765036a1e3f17e13ba961c692b0c5b21
valensixx <valensixx@gmail.com>
2023-05-06 10:33:07 +0300

    test changes from laptop

---
+++ src/MyArrayList.java
@@ -0,0 +1,17 @@
+//import java.util.ArrayList;
+
+//public class MyArrayList {
+    //public static void main(String[] args) {
+        // ArrayList is resizable array - elements can be added and removed after compilation phase.
+        // It stoe reference data types
+        // Difference between primitive and wrapper data types is that wrapper starts with capital letter:
+        // (boolean,char,int,double) == (Boolean, Char, Int, Double)
+
+        /*
+         * Example of wrapper data types: Boolean a= true; Character b='@'; Double c=
+         * 3.14; Integer d = 123; String e = "Name";
+         */
+
+        ///ArrayList<String> food = new ArrayList<String>();
+   // }
+//}
